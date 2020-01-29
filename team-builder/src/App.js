6992ac65621f9fb6390846d589314623c39d7ab8
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2020-01-29 16:25:16 -0700

    MVP Complete.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import Members from './components/Members';
+import memberData from './memberData';
 
 function App() {
-  const [members, setMembers] = useState([]);
-
+  const [members, setMembers] = useState(memberData);
+  const addNewMember = member => {
+    const newMember = {
+      id: Date.now(),
+      name: member.name,
+      email: member.email,
+      role: member.role
+    };
+    setMembers([...members, newMember])
+  }
   return (
     <div className="App">
-      <h1>Testing!</h1>
+      <h1>team-builder!</h1>
+      <Members members={members} />
+      <Form addNewMember={addNewMember} />
     </div>
   );
 }
